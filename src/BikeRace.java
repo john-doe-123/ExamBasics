911e80dbd96d23720c6c476baad0b75cfd030a55
john-doe-123 <john.doe123@abv.bg>
2017-05-04 12:51:16 +0300

    Added problem links

--- src/BikeRace.java
+++ src/BikeRace.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by pesho on 5/4/2017.
  */
+
+// BikeRace requirements -> https://judge.softuni.bg/Contests/Practice/DownloadResource/1728
+// BikeRace Judge        -> https://judge.softuni.bg/Contests/Practice/Index/359#2
 public class BikeRace {
 
     public static void main(String[] args) {
 
-        // test
+        Scanner userInput = new Scanner(System.in);
+
+
     }
 
 }
