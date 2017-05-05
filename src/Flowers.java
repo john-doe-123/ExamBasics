9a74ab2bb67ca87898d91ac2c7270e0495f99f3e
john-doe-123 <john.doe123@abv.bg>
2017-05-05 11:52:29 +0300

    Added Test.java to gitignore

---
+++ src/Flowers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by pesho on 5/4/2017.
+ */
+public class Flowers {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        int chrysanthemumQuantity = Integer.parseInt(userInput.nextLine());
+        int roseQuantity = Integer.parseInt(userInput.nextLine());
+        int tulipQuantity = Integer.parseInt(userInput.nextLine());
+        String season = userInput.nextLine();
+        char isHoliday = userInput.nextLine().charAt(0);
+
+
+    }
+}
