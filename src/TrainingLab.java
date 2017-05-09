beb6b8c39bd56451d05162936ec6c45b5b27ed97
john-doe-123 <john.doe123@abv.bg>
2017-05-09 20:17:34 +0300

    Finished TrainingLab

---
+++ src/TrainingLab.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+    /**
+     * Created by pesho on 5/9/2017.
+     */
+    public class TrainingLab {
+
+        public static void main(String[] args) {
+
+            Scanner userInput = new Scanner(System.in);
+
+            double width = Double.parseDouble(userInput.nextLine());
+            double height = Double.parseDouble(userInput.nextLine());
+
+            double heightInCM = height * 100;
+            heightInCM -= 100; // 100 cm needed for corridor.
+
+            int numDesks = (int)(heightInCM / 70);
+            double widthInCM = width * 100;
+            int numRows = (int)widthInCM / 120;
+
+            int numPlaces = numDesks * numRows - 3;
+            System.out.println("numPlaces = " + numPlaces);
+        }
+    }
+
+}
